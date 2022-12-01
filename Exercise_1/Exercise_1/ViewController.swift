66393a8ebd913d408d6d7ea59bd9ecf878f033cf
Dominic(Dongkyu)Lee <ciuizz@naver.com>
2022-12-01 22:46:02 +0900

    first touch
    initialize project

---
+++ Exercise_1/Exercise_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exercise_1
+//
+//  Created by 이동규 on 2022/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
